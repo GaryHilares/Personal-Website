b7a867c8cf81f959fd6f832ecb8bc560f430b96c
Gary Hilares <dev.garyhilares@gmail.com>
2025-06-13 05:01:16 +0000

    Added WIP, reorganized content, and improved hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,36 @@ interface LinkData {
   url: string;
 }
 
-function Hero({title, subtitle, links}: {title: string, subtitle: string, links: Array<LinkData>}) {
+function WIPHeader() {
+  return (
+    <header className="bg-black text-white p-3">
+      This website is currently under construction. Come back soon to see updates!
+    </header>
+  );
+}
+
+function Hero({title, subtitle, subsubtitle, src, links}: {title: string, subtitle: string, subsubtitle: string, src: string, links: Array<LinkData>}) {
   return (
     <div className="min-h-screen flex">
       <div className="m-auto">
         <h1 className="text-center font-bold text-4xl">{title}</h1>
-        <p className="text-center font-bold text-lg">{subtitle}</p>
+        <p className="text-center font-bold text-xl">{subtitle}</p>
+        <p className="text-center text-lg">{subsubtitle}</p>
         <ul className="text-center">
           {links.map((link, idx) => (
             <li key={idx} className="inline-block mx-3">
               <a
                 href={link.url}
-                className="underline text-blue-700"
+                className="underline"
               >
                 {link.text}
               </a>
             </li>
           ))}
         </ul>
+        <div className="my-2">
+          <img src={src} width="200" heigh="200" className="m-auto" />
+        </div>
       </div>
     </div> 
   )
@@ -28,8 +40,8 @@ function Hero({title, subtitle, links}: {title: string, subtitle: string, links:
 
 function Section({title, children}: {title: string, children: React.ReactNode}) {
   return (
-    <div>
-      <h2 className="font-bold">{title}</h2>
+    <div className="m-3">
+      <h2 className="font-bold text-xl">{title}</h2>
       {children}
     </div>
   )
@@ -66,11 +78,14 @@ function ProjectCard({name, url, date, technologies, bulletPoints}: {
 export default function Home() {
   return (
     <>
+      <WIPHeader />
       <main className="m-3">
         <article>
           <Hero
             title="Gary Hilares"
             subtitle="All-rounder software developer"
+            subsubtitle="Principled, passionate, user-focused programming"
+            src="https://avatars.githubusercontent.com/u/46727048?s=400&u=9bdce7cb9eabd7d51bebbf77fd755a3c38ac873a&v=4"
             links={[
               {text: "GitHub", url: "https://github.com/GaryHilares"},
               {text: "LinkedIn", url: "https://www.linkedin.com/in/gary-hilares/"},
@@ -80,7 +95,7 @@ export default function Home() {
           <Section title="Technical skills">
             <p>
               <span className="underline">Languages:</span> TypeScript,
-              JavaScript, Node.js, Python, R, CSS3, HTML5, C++, C, Java
+              JavaScript, Node.js, Python, R, CSS3, HTML5, Rust, C++, C, Java
             </p>
             <p>
               <span className="underline">Web frameworks:</span> Next.js,
@@ -127,49 +142,52 @@ export default function Home() {
               ]}
             />
           </Section>
-          <h2 className="font-bold underline">Contests</h2>
-          <h3 className="font-bold">
-            1st place in the ICPC PacNW 2024 Div. 2 contest
-          </h3>
-          <p className="italic">September 2024 - April 2025</p>
-          <p>At Simon Fraser University, Burnaby, BC</p>
-          <ul className="list-disc ms-4">
-            <li>
-              Won 1st place out of 85 teams in the International Collegiate
-              Programming Contest PacNW 2024 Div. 2.
-            </li>
-            <li>
-              Cooperatively solved challenging problems in topics like dynamic
-              programming with two other UBC students.
-            </li>
-          </ul>
-          <h2 className="font-bold underline">Work experience</h2>
-          <h3 className="font-bold">Computer science teaching assistant</h3>
-          <p className="italic">September 2024 - April 2025</p>
-          <p>At University of British Columbia, Vancouver, BC</p>
-          <ul className="list-disc ms-4">
-            <li>
-              Worked with 1-2 other teaching assistants to hold 40+ 3-hour lab
-              sessions on 10 topics related to systematic program design with
-              5-30 students each.
-            </li>
-            <li>
-              Assisted students to learn through one-to-one conversations held
-              during my 1-hour-long and 2-hour-long weekly office hours.
-            </li>
-          </ul>
-          <h2 className="font-bold underline">Education</h2>
-          <h3 className="font-bold">Bachelor of Science, Computer Science</h3>
-          <p className="italic">September 2023 - May 2028 (expected)</p>
-          <p>At University of British Columbia, Vancouver, BC</p>
-          <ul className="list-disc ms-4">
-            <li>GPA: 94.8% (73 credits so far).</li>
-            <li>
-              Awarded the IMES scholarship, given to exceptional international
-              students entering UBC based on academic excellence and community
-              involvement
-            </li>
-          </ul>
+          <Section title="Work Experience">
+            <h3 className="font-bold">Computer science teaching assistant</h3>
+            <p className="italic">September 2024 - April 2025</p>
+            <p>At University of British Columbia, Vancouver, BC</p>
+            <ul className="list-disc ms-4">
+              <li>
+                Worked with 1-2 other teaching assistants to hold 40+ 3-hour lab
+                sessions on 10 topics related to systematic program design with
+                5-30 students each.
+              </li>
+              <li>
+                Assisted students to learn through one-to-one conversations held
+                during my 1-hour-long and 2-hour-long weekly office hours.
+              </li>
+            </ul>
+          </Section>
+          <Section title="Contests & Awards">
+            <h3 className="font-bold">
+              1st place in the ICPC PacNW 2024 Div. 2 contest
+            </h3>
+            <p className="italic">September 2024 - April 2025</p>
+            <p>At Simon Fraser University, Burnaby, BC</p>
+            <ul className="list-disc ms-4">
+              <li>
+                Won 1st place out of 85 teams in the International Collegiate
+                Programming Contest PacNW 2024 Div. 2.
+              </li>
+              <li>
+                Cooperatively solved challenging problems in topics like dynamic
+                programming with two other UBC students.
+              </li>
+            </ul>
+          </Section>
+          <Section title="Education & Certifications">
+            <h3 className="font-bold">Bachelor of Science, Computer Science</h3>
+            <p className="italic">September 2023 - May 2028 (expected)</p>
+            <p>At University of British Columbia, Vancouver, BC</p>
+            <ul className="list-disc ms-4">
+              <li>GPA: 94.8% (73 credits so far).</li>
+              <li>
+                Awarded the IMES scholarship, given to exceptional international
+                students entering UBC based on academic excellence and community
+                involvement
+              </li>
+            </ul>
+          </Section>
         </article>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>

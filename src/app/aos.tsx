c05ca795dfb0c125d363bbb47176cc36cbf83b40
Gary Hilares <dev.garyhilares@gmail.com>
2025-06-30 14:37:20 -0500

    Removed unused import

--- src/app/aos.tsx
+++ src/app/aos.tsx
@@ -2,7 +2,7 @@
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-import { ReactNode, useEffect } from "react";
+import { useEffect } from "react";
 
 function AosInitializer() {
   useEffect(() => {

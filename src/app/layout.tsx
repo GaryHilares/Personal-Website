0c3ce276e0313c0747159cd72e8862887e264330
Gary Hilares <dev.garyhilares@gmail.com>
2025-07-01 23:44:24 -0500

    Added GoogleAnalytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -37,6 +38,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleAnalytics gaId="G-9FQ4VVYNTH" />
     </html>
   );
 }
